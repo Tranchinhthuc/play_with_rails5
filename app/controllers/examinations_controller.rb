83519c2fb1e09c3fe37a1b678cacd888a9bd3001
Tran Chinh Thuc <thuctc91@gmail.com>
2016-10-10 17:45:10 +0700

    Bug import

--- app/controllers/examinations_controller.rb
+++ app/controllers/examinations_controller.rb
@@ -9,7 +9,12 @@ class ExaminationsController < ApplicationController
 
   def buy_examination
     examination = Examination.find params[:id]
-    current_user.buy_examination(examination)
-    redirect_to(params[:redirect] == 'true' ? new_examination_answer_sheet_path(examination) : :back)
+    if current_user.point >= examination.point
+      current_user.buy_examination(examination)
+      redirect_to(params[:redirect] == 'true' ? new_examination_answer_sheet_path(examination) : :back)
+    else
+      flash[:alert] = "Không đủ point rồi chế ơi!"
+      redirect_to :back
+    end
   end
 end
