6247dcec80c1dbf406cb8d3a45ac7f61ed36e8be
Tran Chinh Thuc <thuctc91@gmail.com>
2016-10-16 08:51:31 +0700

    revert database.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :store_current_location, :unless => :devise_controller?
+
   include CanCan::ControllerAdditions
   layout :select_layout
 
@@ -22,8 +24,12 @@ class ApplicationController < ActionController::Base
   def select_layout
     if current_user.present?
       "admin_lte_2" unless current_user.normal?
-    # else
-    #   "has_not_login"
+    else
+      "has_not_login"
     end
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,7 +8,7 @@ class WordsController < ApplicationController
   end
 
   def show
-    render json: word
+    render json: @word
   end
 
   def create
@@ -21,40 +21,40 @@ class WordsController < ApplicationController
   end
 
   def update
-    if word.update_attributes(word_params)
-      render json: word
+    if @word.update_attributes(word_params)
+      render json: @word
     else
-      render json: {error: word.errors.messages}, status: 422
+      render json: {error: @word.errors.messages}, status: 422
     end
   end
 
   def update_meaning
-    if word.update_attributes(meaning: params[:meaning])
-      render json: word
+    if @word.update_attributes(meaning: params[:meaning])
+      render json: @word
     else
-      render json: {error: word.errors.messages}, status: 422
+      render json: {error: @word.errors.messages}, status: 422
     end
   end
 
   def update_status
-    if word.update_attributes(meaning: params[:status])
-      render json: word
+    if @word.update_attributes(status: params[:status])
+      render json: @word
     else
-      render json: {error: word.errors.messages}, status: 422
+      render json: {error: @word.errors.messages}, status: 422
     end
   end
 
   def update_origin
-    if word.update_attributes(meaning: params[:origin])
-      render json: word
+    if @word.update_attributes(meaning: params[:origin])
+      render json: @word
     else
-      render json: {error: word.errors.messages}, status: 422
+      render json: {error: @word.errors.messages}, status: 422
     end
   end
 
   def destroy
-    if word.destroy
-      render json: word
+    if @word.destroy
+      render json: @word
     else
       render json: {error: "Can't delete this word"}, status: 422
     end
@@ -62,10 +62,10 @@ class WordsController < ApplicationController
 
   private
   def word_params
-    params.require(:sensor).permit(:origin, :meaning, :status)
+    params.require(:word).permit(:origin, :meaning, :status)
   end
 
   def find_word
-    word = Word.find params[:id]
+    @word = Word.find params[:id]
   end
 end
